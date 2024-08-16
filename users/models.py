947dcd11ecbd3800f75ccf11413f9cd36b829db1
lucy2003fer <lucy48fernada@gmail.com>
2024-08-16 10:30:12 -0500

    Cargando base de datos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Persona(models.Model):
+    
+    ESTADO_CHOICE = [
+        ('activo', 'Activo'),
+        ('inactivo', 'Inactivo')
+    ]
+    nombre = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    fecha_Creacion = models.DateField(auto_now=True)
+    estado = models.CharField(max_length=50, choices=ESTADO_CHOICE, default='activo')
